ef4ce1867786d011e7767755dab474a600305d20
Your Name <you@example.com>
2023-08-08 22:06:23 +0100

    task 2 😎

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast;
+	listint_t *slow;
+
+	fast = list;
+	slow = list;
+
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
+	}
+	return (0);
+}
