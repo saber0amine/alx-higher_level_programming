8528c2598bbf5b4b0a5ba81a1d5c3dbcceea27ab
Your Name <you@example.com>
2023-08-08 23:17:10 +0100

    add all files and fix error 2 😎

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z')+1):
-    print("{}".format(chr(char)), end='')
+for a in range(ord("a"), ord("z") + 1):
+    print("{:s}".format(chr(a)), end="")
