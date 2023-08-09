d5513ff94ef3e9506dc8a3a77efe42e8768a43d3
Your Name <you@example.com>
2023-08-09 14:53:38 +0100

    add all files and fix error tasks 😎

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# Author - Saber Amine
 for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
         print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+# 6-print_comb3.py
 
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        if i != 8:
-            print("{:d}{:d}".format(i, j), end=', ')
+"""Print all possible different combinations of two digits in ascending order.
+The two digits must be different - 01 and 10 are considered identical."""
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
         else:
-            print("{:d}{:d}".format(i, j))
+            print("{}{}".format(digit1, digit2), end=", ")
