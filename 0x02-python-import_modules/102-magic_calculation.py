3b4396ab3ca987dbb8c0b3a1f98f7418089f4ee8
Your Name <you@example.com>
2023-08-11 01:15:32 +0100

    add all files II 😎

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
+    """Match bytecode provided"""
     from magic_calculation_102 import add, sub
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
+        return (c)
 
-    return sub(a, b) if 1 else None
+    else:
+        return (sub(a, b))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
+
 if __name__ == "__main__":
-    argc = len(argv)
-    if argc > 1:
-        count = 0
-        if argc > 2:
-            print("{:d} arguments:".format(argc - 1))
-        else:
-            print("{:d} argument:".format(argc - 1))
-        for arg in argv:
-            count += 1
-            if count != 1:
-                print("{:d}: {}".format(count - 1, arg))
-    else:
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
